42fee87a830c084494aaab60c6a36b1f16d7a79a
K.D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2018-07-14 19:55:52 +0530

    Readded manual handling of form empty fields

--- js/form-validate.js
+++ js/form-validate.js
@@ -30,6 +30,7 @@ function validate() {
 
   var fromControls = $(".form-control");
 
+  var emptyText = $("#empty_text");
   var validText = $("#valid_text");
   var invalidText = $("#invalid_text");
 
@@ -38,12 +39,26 @@ function validate() {
   validText.css("display", "none");
   invalidText.css("display", "none");
 
+  // Check for empty field
+  var isEmptyField = false;
+  fromControls.each(function(index, element) {
+    if ($(element).val() === "") {
+      $(element).addClass("is-invalid");
+      invalidText.css("display", "inline");
+      $(element).get(0).scrollIntoView();
+      isEmptyField = true;
+      return false;
+    }
+  });
+  if (isEmptyField) return false;
+
   // Chack if 2 dates are valid
   if (startDate.val() > endDate.val()) {
     // Start Date Greater than End Date
     startDate.addClass("is-invalid");
     endDate.addClass("is-invalid");
     invalidText.css("display", "inline");
+    startDate.get(0).scrollIntoView();
     return false;
   }
 
@@ -53,6 +68,7 @@ function validate() {
     startTime.addClass("is-invalid");
     endTime.addClass("is-invalid");
     invalidText.css("display", "inline");
+    startTime.get(0).scrollIntoView();
     return false;
   }
 
